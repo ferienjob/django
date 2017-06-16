8f517e412797ac3208779069aba72fe5da5ab679
Tobias Riepe <tobias.riepe@gmx.de>
2017-06-16 14:35:25 +0200

    update

--- FerienJobsBoerse/urls.py
+++ FerienJobsBoerse/urls.py
@@ -20,5 +20,6 @@ from FerienJobsBoerse import views
 
 urlpatterns = [
     url(r'^admin/', (admin.site.urls)),
-    ##url(r'^$' ,views.index)
+    url(r'^$' ,views.start),
+    url(r'^suche/$' ,views.suche)
 ]
