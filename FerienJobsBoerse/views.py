a6e05e1e3fa64f2ac037f5016b11ee248056bdc7
Tobias Riepe <tobias.riepe@gmx.de>
2017-06-16 19:54:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- FerienJobsBoerse/views.py
+++ FerienJobsBoerse/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.template import Context , loader
 from django.shortcuts import render
-
+import django.FerienJobsBoerse.models as models
 # def base(request):
 #     return render(request, '../templates/base.html', {'message':'Hello Word 123'})
 
 def start(request):
+
     return render(request, '../templates/home.html', {'message':'Hello Word 123'})
+
 def suche(request):
     return render(request, '../templates/suche.html', {'message':'Hello Word 123'})
+
+def login(request):
+    return render(request, '../templates/login.html', {models.Person.email,models.Person.password})
