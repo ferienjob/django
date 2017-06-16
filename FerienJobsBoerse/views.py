1fd220168ed898afc9aa4c878118980343dcec03
JohannesKaufmann <johannes@joina.de>
2017-06-16 19:11:47 +0200

    added the newest version of the landing page

--- FerienJobsBoerse/views.py
+++ FerienJobsBoerse/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from django.template import Context , loader
 from django.shortcuts import render
 
-def base(request):
-    return render(request, '../templates/base.html', {'message':'Hello Word 123'})
+# def base(request):
+#     return render(request, '../templates/base.html', {'message':'Hello Word 123'})
 
 def start(request):
-    return render(request, '../templates/start.html', {'message':'Hello Word 123'})
+    return render(request, '../templates/home.html', {'message':'Hello Word 123'})
 def suche(request):
     return render(request, '../templates/suche.html', {'message':'Hello Word 123'})
