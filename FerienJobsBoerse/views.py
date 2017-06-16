52ce7f82c48eea78b451fd499b2b9de8e1c55a47
JohannesKaufmann <johannes@joina.de>
2017-06-16 20:13:35 +0200

    added navbar

--- FerienJobsBoerse/views.py
+++ FerienJobsBoerse/views.py
@@ -2,10 +2,15 @@ from django.http import HttpResponse
 from django.template import Context , loader
 from django.shortcuts import render
 
+routes = [
+    {'name': 'Home', 'key': '/'},
+    {'name': 'Suche', 'key': '/suche/'}
+]
+
 # def base(request):
 #     return render(request, '../templates/base.html', {'message':'Hello Word 123'})
 
 def start(request):
-    return render(request, '../templates/home.html', {'message':'Hello Word 123'})
+    return render(request, '../templates/home.html', {'routes': routes, 'current_route': '/'})
 def suche(request):
-    return render(request, '../templates/suche.html', {'message':'Hello Word 123'})
+    return render(request, '../templates/suche.html', {'routes': routes, 'current_route': '/suche/'})
