683a0cf101d69ea26867497e5a824e94448b3ce9
JohannesKaufmann <johannes@joina.de>
2017-06-17 09:23:40 +0200

    added job profile

--- FerienJobsBoerse/views.py
+++ FerienJobsBoerse/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.template import Context , loader
 from django.shortcuts import render
 import django.FerienJobsBoerse.models as models
-routes = [
 
+routes = [
     {'name': 'Home', 'key': '/'},
     {'name': 'Suche', 'key': '/suche/'}
 ]
@@ -11,14 +11,27 @@ routes = [
 # def base(request):
 #     return render(request, '../templates/base.html', {'message':'Hello Word 123'})
 
-def start(request):
+
+def home(request):
     return render(request, '../templates/home.html', {'routes': routes, 'current_route': '/'})
 
+
 def suche(request):
+    # Find multiple jobs by title & city
+    job_title = request.GET.get('title', '')
+    branche = request.GET.get('branche', '')
+    ort = request.GET.get('ort', '')
+
+    print(job_title, branche, ort)
+
     return render(request, '../templates/suche.html', {'routes': routes, 'current_route': '/suche/'})
+
+
+def job(request, id):
+    # Find Job by id
+
+    return render(request, '../templates/job.html', {'id': id, 'routes': routes, 'current_route': '/job/'})
+
 
 def login(request):
     return render(request, '../templates/login.html', {models.Person.email,models.Person.password})
-
-def job(request):
-    return render(request, '../templates/job.html', {'routes': routes, 'current_route': '/job/'})
