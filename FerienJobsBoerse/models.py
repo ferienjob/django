1c8f599e8819263da952ce467bd028de1966cddb
JohannesKaufmann <johannes@joina.de>
2017-06-18 10:53:44 +0200

    updated model

--- FerienJobsBoerse/models.py
+++ FerienJobsBoerse/models.py
@@ -6,7 +6,7 @@ class Person(models.Model):
     password = models.CharField(max_length=100)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    address = models.CharField(max_length=30)
+    address = models.CharField(max_length=300)
     telephone_number = models.CharField(max_length=30)
     date_of_birth = models.CharField(max_length=30)
     qualifications = models.CharField(max_length=30)
@@ -16,17 +16,17 @@ class Company(models.Model):
     email = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=100)
     name = models.CharField(max_length=30)
-    homepage = models.CharField(max_length=30)
-    address = models.CharField(max_length=30)
+    homepage = models.CharField(max_length=300)
+    address = models.CharField(max_length=300)
     telephone_number = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
-    image_url = models.CharField(max_length=30)
+    image_url = models.CharField(max_length=300)
 
     contact_first_name = models.CharField(max_length=30)
     contact_last_name = models.CharField(max_length=30)
     contact_email = models.CharField(max_length=30)
     contact_telephone_number = models.CharField(max_length=30)
-    contact_image_url = models.CharField(max_length=30)
+    contact_image_url = models.CharField(max_length=300)
 
     @classmethod
     def create(cls, data):
@@ -36,16 +36,16 @@ class Company(models.Model):
 class Job(models.Model):
     title = models.CharField(max_length=30)
     professional_field = models.CharField(max_length=30)
-    address = models.CharField(max_length= 30)
+    address = models.CharField(max_length=300)
     start_date = models.CharField(max_length= 30)
     end_date = models.CharField(max_length= 30)
     start_time = models.CharField(max_length= 30)
     end_time = models.CharField(max_length= 30)
     min_age = models.CharField(max_length= 30)
     income_per_hour = models.CharField(max_length= 30)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=300)
     requirements = models.CharField(max_length= 30)
-    image_url = models.CharField(max_length= 30)
+    image_url = models.CharField(max_length= 300)
 
     company = models.ForeignKey(
         'Company',
